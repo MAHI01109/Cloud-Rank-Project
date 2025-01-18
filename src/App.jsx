a36a7bf0e8a00f619efc913d730b8974d1793feb
MAHI01109 <mahendrasinghddnuk@gmail.com>
2025-01-18 21:02:55 +0530

    Updated dependencies and app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
+import { useState } from 'react';
 import './App.css'
-
+import SlectUser from './Components/SelectUser'
 function App() {
+  const [selected, setSelected] = useState(null);
   return (
-    <>
-     
-    </>
+    <div className='h-screen'>
+      <header className='flex  justify-center items-center bg-blue-500 h-24'>
+        <div className='w-72'>
+          <SlectUser selected={selected} setSelected={setSelected} />
+        </div>
+
+      </header>
+    </div>
   )
 }
 
